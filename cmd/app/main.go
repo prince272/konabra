1d5476514798d3463d4b98f25c354f54ed54ed45
Prince Owusu <princeowusu.272@gmail.com>
2025-04-18 16:22:33 +0000

    Revert "Refactor application structure with dependency injection"
    This reverts commit 4e1f201ec44323cb6f5260f1eef5da72fc27e7b3.

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,14 +1,13 @@
 package main
 
-import "github.com/prince272/konabra/internal/app"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	// Initialize the application configuration and dependencies
-	app.Init()
-
-	// Set up routes and request handlers
-	app.MapHandlers()
-
-	// Start the application server
-	app.Run()
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run() // listen and serve on 0.0.0.0:8080
 }
