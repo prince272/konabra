ab801e421d115406e22de48bb842c478834ff072
Prince Owusu <princeowusu.272@gmail.com>
2025-05-17 13:56:44 +0000

    Add Prettier configuration and update scripts
    - Updated `package.json` to include new linting and formatting scripts.
    - Modified `.prettierignore` to specify files and directories to ignore.
    - Added `prettier.config.js` with custom formatting rules and import order.

---
+++ apps/next/prettier.config.js
@@ -0,0 +1,35 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  endOfLine: "auto",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "none",
+  printWidth: 100,
+  plugins: [],
+  importOrder: [
+    "^.+\\.(css|scss)$",
+    "^(react/(.*)$)|^(react$)",
+    "^(react-native/(.*)$)|^(react-native$)",
+    "^(next/(.*)$)|^(next$)",
+    "<THIRD_PARTY_MODULES>",
+    "^@/configs/(.*)$",
+    "^@/constants/(.*)$",
+    "^@/types/(.*)$",
+    "^@/lib/(.*)$",
+    "^@/utils/(.*)$",
+    "^@/states/(.*)$",
+    "^@/services/(.*)$",
+    "^@/hooks/(.*)$",
+    "^@/components/ui/(.*)$",
+    "^@/components/(.*)$",
+    "^@/app/(.*)$",
+    "^[./]"
+  ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"]
+};
+
+module.exports = config;
